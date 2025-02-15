2c52701c4ae06514e253184b473b2af01193d037
Natan Camargos <natanscamargos@gmail.com>
2025-02-15 16:30:31 -0300

    fix /docs

--- index.test.js
+++ index.test.js
@@ -3,4 +3,4 @@ var tests = require('./index.js');
 const sum = (a, b) => a + b;
 
 tests.equals(1, 1, "1 equals 1");
-tests.equals(sum(1, 1), 2, "sum(1, 1) === 2")
+tests.equals(sum(1, 1), 2, "sum(1, 1) === 2");
